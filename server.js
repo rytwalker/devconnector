8c39273b86d5a57b5fab854aed9e31e97d308760
Ryan Walker <rytwalker@gmail.com>
2019-05-05 19:08:29 -0400

    setup routes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.send('API Running');
 });
 
+// Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`\nServer started on port: ${PORT}\n`));
